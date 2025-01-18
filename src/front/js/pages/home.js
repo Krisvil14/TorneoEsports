c238f9647edbfdb8044f4ca0f18045a4c517a2cb
andlicon <andresjzabalac@hotmail.com>
2025-01-18 13:08:40 -0400

    feat(client): :sparkles: dark mode

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -16,7 +16,7 @@ export const Home = () => {
         {store.message ||
           'Loading message from the backend (make sure your python backend is running)...'}
       </div>
-      <P className="xd">
+      <P className="xd" textColor="primary">
         This boilerplate comes with lots of documentation:{' '}
         <a href="https://start.4geeksacademy.com/starters/react-flask">
           Read documentation
