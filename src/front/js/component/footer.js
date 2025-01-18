3b1f031b9bbee390ce6632a9ddd93f25345917eb
andlicon <andresjzabalac@hotmail.com>
2025-01-18 18:42:35 -0400

    fix(client): :bug: new elements in whole app

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,14 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import Container from './html/Container';
+import Text from './html/Text';
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+  <Container as="footer" className="footer mt-auto py-3 text-center">
+    <Text>
+      Made with <Text as="i" className="fa fa-heart text-danger" /> by{' '}
+      <Text as="a" href="http://www.4geeksacademy.com">
+        4Geeks Academy
+      </Text>
+    </Text>
+  </Container>
 );
