939a32c4060fd5ca47c1dcf52ae9e133927911ec
Krisvil14 <gabbo1162@gmail.com>
2025-02-10 21:03:40 -0400

    feat(api): Arregle el databe_url y cree el modelo de usuario

--- src/api/models.py
+++ src/api/models.py
@@ -4,9 +4,14 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    cedula = db.Column(db.String(20), unique=True, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    role = db.Column(db.String(20), nullable=False)  #Aqui como se haria para que solo se pueda escoger entre los roles de usuario o admin?
+    is_active = db.Column(db.Boolean(), nullable=False, default=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -14,6 +19,12 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "cedula": self.cedula,
+            "age": self.age,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "role": self.role,
         }
+    
+    
