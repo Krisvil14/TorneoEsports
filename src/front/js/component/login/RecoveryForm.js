f44d5804e5aba06487d4e1408643c4dd0fe2eb4b
Krisvil14 <gabbo1162@gmail.com>
2025-02-23 10:58:44 -0400

    feat: :sparkles: Se creo el endpoint de HU-3 y se integro el formulario

---
+++ src/front/js/component/login/RecoveryForm.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+
+export default function RecoveryForm() {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('email', email);
+    formData.append('password', password);
+
+    const notification = toast.loading('Recuperando usuario...');
+    
+        try {
+          const response = await fetch(process.env.BACKEND_URL + '/api/login', {
+            method: 'POST',
+            body: formData,
+          });
+          const { ok } = response;
+          const json = await response.json();
+    
+          if (ok) {
+            toast.update(notification, {
+              render: 'Correo enviado',
+              type: 'success',
+              autoClose: 5000,
+              isLoading: false,
+            });
+          } else {
+            toast.update(notification, {
+              render: json.error,
+              type: 'error',
+              autoClose: 5000,
+              isLoading: false,
+            });
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      };
+
+      return (
+        <form onSubmit={handleSubmit}>
+          <div className="row mb-4 text-center">
+            <h1 className="w-75 mx-auto fs-1">Recuperar usuario</h1>
+          </div>
+          <div className="row gy-3">
+            <div className="d-flex flex-column gy-3 w-75 mx-auto form-group">
+              <label for="email">Correo:</label>
+              <input
+                type="text"
+                id="email"
+                name="email"
+                value={email}
+                onChange={({ target }) => setEmail(target.value)}
+                required
+                className="form-control"
+                />
+            </div>
+            <div className="d-flex flex-column gy-3 w-75 mx-auto">
+              <label for="password">Contraseña:</label>
+              <input
+                onChange={({ target }) => setPassword(target.value)}
+                value={password}
+                className="form-control"
+                type="password"
+                id="password"
+                name="password"
+                required
+              />
+            </div>
+            <button type="submit" class="btn btn-primary w-75 mx-auto">
+            Primary
+            </button>
+            </div>
+        </form>
+        );
+};
