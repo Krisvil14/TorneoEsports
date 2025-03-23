d842f1672ee86b926983344e8f3d7c68bcda7c3a
Krisvil14 <gabbo1162@gmail.com>
2025-03-22 20:50:18 -0400

    feat: :sparkles: Se arreglo en navbar y se hizo la validacion para que siempre que se vaya al login se cierre la sesion

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -4,6 +4,7 @@ import Container from './html/Container';
 import Text from './html/Text';
 import { Context } from '../store/appContext';
 import { useNavigate } from 'react-router-dom';
+import controlImageUrl from '../../img/control.png';
 
 export const Navbar = () => {
   const { store, actions } = useContext(Context);
@@ -11,33 +12,44 @@ export const Navbar = () => {
 
   const handleLogout = () => {
     actions.logout();
-    navigate('/login');
+    navigate('/login', { replace: true });
   };
 
   return (
-    <Container as="nav" className="navbar navbar-light bg-light">
-      <Container className="container">
-        <Link to="/">
-          <Text as="span" className="navbar-brand mb-0 h1">
-            React Boilerplate
-          </Text>
-        </Link>
-        <Container className="ml-auto">
-          <Link to="/inicio">
-            <button className="btn btn-primary">
-              Inicio
-            </button>
-          </Link>
-          <Link to="/demo">
-            <button className="btn btn-primary ml-2">
-              Check the Context in action
-            </button>
-          </Link>
-          {store.isAuthenticated && (
-            <button onClick={handleLogout} className="btn btn-danger ml-2">
-              Cerrar Sesión
-            </button>
-          )}
+    <Container as="nav" className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: 'purple' }}>
+      <Container className="container-fluid">
+        <div style={{ height: '56px', display: 'flex', alignItems: 'center' }}>
+          <img src={controlImageUrl} style={{ height: '100%' }} />
+        </div>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <Container className="collapse navbar-collapse" id="navbarNav">
+          <Container className="navbar-nav justify-content-center w-100">
+            {store.isAuthenticated && (
+              <Container className="d-flex">
+                <Link to="/inicio" className="nav-item nav-link" style={{ marginRight: '10px' }}>
+                  <Text style={{ fontSize: '1.2rem', fontFamily: 'Impact, sans-serif', color:'' }}>Inicio</Text>
+                </Link>
+                <Link to="/teams" className="nav-item nav-link" style={{ marginRight: '10px' }}>
+                  <Text style={{ fontSize: '1.2rem', fontFamily: 'Impact, sans-serif',color:'' }}>Equipos</Text>
+                </Link>
+                <Link to="/users" className="nav-item nav-link" style={{ marginRight: '10px' }}>
+                  <Text style={{ fontSize: '1.2rem', fontFamily: 'Impact, sans-serif',color:'' }}>Usuarios</Text>
+                </Link>
+                <Link to="/tournaments" className="nav-item nav-link" style={{ marginRight: '10px' }}>
+                  <Text style={{ fontSize: '1.2rem', fontFamily: 'Impact, sans-serif',color:'' }}>Torneos</Text>
+                </Link>
+              </Container>
+            )}
+            {store.isAuthenticated && (
+              <Container className="ms-auto">
+                <button onClick={handleLogout} className="btn btn-danger" style={{ marginLeft: '10px' }}>
+                <Text style={{ fontSize: '1.2rem', fontFamily: 'Impact, sans-serif',color:'white' }}>Cerrar Sesión</Text>
+                </button>
+              </Container>
+            )}
+          </Container>
         </Container>
       </Container>
     </Container>
